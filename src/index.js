95cffd230071ac253f72ffb81e5e113ad68d5854
Gabriel Marques <gabriel_frahm@hotmail.com>
2021-03-10 08:51:35 -0300

    chore: middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,67 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user){
+    return response.status(404);
+  }
+
+  request.user = user;
+  
+  return next();
+
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  if(user.pro === true) return next();
+  if(user.pro === false && user.todos.length < 10) return next();
+  if(user.pro === false && user.todos.length === 10) return response.status(403);
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user){
+    return response.status(404);
+  }
+
+  const checkUuid = validate(id);
+  
+  if(!checkUuid){
+    return response.status(400);
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo){
+    return response.status(404);
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if(!user){
+    return response.status(404);
+  }
+
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
